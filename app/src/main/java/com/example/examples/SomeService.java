330d7d1c7dcbf68909febee235f0f227cbb41aa7
7enSmile <68625468+7enSmile@users.noreply.github.com>
2020-11-15 00:07:23 +0700

    сервис доделан

--- app/src/main/java/com/example/examples/SomeService.java
+++ app/src/main/java/com/example/examples/SomeService.java
@@ -4,6 +4,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
@@ -18,6 +19,8 @@ import java.util.concurrent.TimeUnit;
 public class SomeService extends Service {
     private static final String LOG_TAG ="hs" ;
     Context context;
+    Thread thread;
+    boolean running=true;
 
     public void onCreate() {
 
@@ -44,31 +47,45 @@ public class SomeService extends Service {
     }
     public void onDestroy() {
         Toast.makeText(this, "Служба остановлена",Toast.LENGTH_SHORT).show();
+
+        running=false;
         super.onDestroy();
 
     }
 
     private void startWork()  {
-
-        new Thread(new Runnable() {
+        thread=new Thread(new Runnable() {
             @Override
             public void run() {
                 for(int timer=0;timer<100;timer++){
+                    if(!running){
+                        break;
+                    }
+
+
                     Log.d("timer", String.valueOf(timer));
                     try {
                         TimeUnit.SECONDS.sleep(1);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                        break;
+
                     }
 
+
                 }
-                stopSelf();
+
             }
-        }).start();
+
+        });
+        thread.start();
+
+
 
 
     }
+
+
+
 
 
 
